ea4c25641981f9f520e411fc9243efec2f90bfaa
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2023-03-15 01:43:43 +0300

    Initial commit

---
+++ InMemory.Caching/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IMemoryCache _memoryCache;
+        
+        public ProductsController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
