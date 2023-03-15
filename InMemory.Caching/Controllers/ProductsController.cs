a56f6b79926606a4e7cec29e14498e3fbe8838a1
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2023-03-15 03:01:34 +0300

    RegisterPostEvictionCallback was implemented.

--- InMemory.Caching/Controllers/ProductsController.cs
+++ InMemory.Caching/Controllers/ProductsController.cs
@@ -20,6 +20,11 @@ namespace InMemory.Caching.Controllers
 
             options.SlidingExpiration = TimeSpan.FromSeconds(10);
             options.Priority = CacheItemPriority.High;
+            
+            options.RegisterPostEvictionCallback((key, value, reason, state) =>
+            {
+                _memoryCache.Set("callback", $"{key}->{value} => sebep:{reason}");
+            });
 
             _memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
 
@@ -29,8 +34,9 @@ namespace InMemory.Caching.Controllers
         public IActionResult Show()
         {
             _memoryCache.TryGetValue("zaman", out string zamancache);
-
+            _memoryCache.TryGetValue("callback", out string callback);
             ViewBag.zaman = zamancache;
+            ViewBag.callback = callback;
 
             return View();
         }
