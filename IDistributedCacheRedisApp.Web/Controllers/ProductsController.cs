f1212d220d8efa59d35e4e1a7f80dddf585394e6
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2023-03-17 03:52:56 +0300

    IDistributed Complex types caching

--- IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using IDistributedCacheRedisApp.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
 
@@ -19,15 +22,30 @@ namespace IDistributedCacheRedisApp.Web.Controllers
         {
             DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
 
-            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
-            
-            await _distributedCache.SetStringAsync("surname", "Bahtiyar",cacheEntryOptions);
+            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(30);
+
+            Product product = new Product { Id = 1, Name = "Kalem", Price = 100 };
+
+            string jsonproduct = JsonSerializer.Serialize(product);
+
+            Byte[] byteproduct = Encoding.UTF8.GetBytes(jsonproduct);
+
+            _distributedCache.Set("product:1", byteproduct);
+
+            //await _distributedCache.SetStringAsync("product:1", jsonproduct, cacheEntryOptions);
+
             return View();
         }
-        
-        public async Task<IActionResult> Show()
+
+        public IActionResult Show()
         {
-            ViewBag.Value = await _distributedCache.GetStringAsync("surname");
+            Byte[] byteProduct = _distributedCache.Get("product:1");
+
+            string jsonproduct = Encoding.UTF8.GetString(byteProduct);
+
+            Product p = JsonSerializer.Deserialize<Product>(jsonproduct);
+
+            ViewBag.product = p;
             return View();
         }
         public async Task<IActionResult> Remove()
