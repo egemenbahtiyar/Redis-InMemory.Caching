e2e24bc01920f4b7b600efd39693bc553631de51
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2023-03-17 03:11:23 +0300

    IDistributed Cache Set and remove operations

--- IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
 
 namespace IDistributedCacheRedisApp.Web.Controllers
@@ -13,8 +15,24 @@ namespace IDistributedCacheRedisApp.Web.Controllers
         }
 
         // GET
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
+
+            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+            
+            await _distributedCache.SetStringAsync("surname", "Bahtiyar",cacheEntryOptions);
+            return View();
+        }
+        
+        public async Task<IActionResult> Show()
+        {
+            ViewBag.Value = await _distributedCache.GetStringAsync("surname");
+            return View();
+        }
+        public async Task<IActionResult> Remove()
+        {
+            await _distributedCache.RemoveAsync("surname");
             return View();
         }
     }
